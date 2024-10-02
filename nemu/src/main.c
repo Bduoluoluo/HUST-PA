394a3334df628307d41a0b6a483935c64266f509
tracer-ics2019 <tracer@njuics.org>
2024-10-02 14:49:49 +0800

    >  compile U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  14:49:49 up  2:16,  0 user,  load average: 0.00, 0.01, 0.00 c0e0db61fb72faf1daf4cc973b08d278755f2ae

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -7,15 +7,8 @@ int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
 
-  // /* Receive commands from user. */
-  // ui_mainloop(is_batch_mode);
-
-  char buf[65536];
-  scanf("%s", buf);
-  bool success;
-  uint32_t val = expr(buf, &success);
-  if (success == false) printf("bad expression!\n");
-  else printf("%u\n", val);
+  /* Receive commands from user. */
+  ui_mainloop(is_batch_mode);
 
   return 0;
 }

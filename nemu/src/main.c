ca5481718244d3ee99709328ebfd26685b54e18d
tracer-ics2019 <tracer@njuics.org>
2024-10-02 14:10:30 +0800

    >  compile U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  14:10:30 up  1:37,  0 user,  load average: 0.00, 0.02, 0.00 a7a3ee01ec79934816fea80d1e7a46e89ed3ddf9

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]) {
   // /* Receive commands from user. */
   // ui_mainloop(is_batch_mode);
 
-  char buf[1024] = "(1)";
+  char buf[1024] = "1+1";
   // scanf("%s", buf);
   bool success;
   uint32_t val = expr(buf, &success);

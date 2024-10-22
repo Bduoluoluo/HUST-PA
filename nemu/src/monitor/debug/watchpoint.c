58f21f62ab1e19cd9835249e5cbc7674758db474
tracer-ics2019 <tracer@njuics.org>
2024-10-22 12:10:01 +0800

    >  compile U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  12:10:01 up 37 min,  0 user,  load average: 0.03, 0.03, 0.00 955fd2d4b1646b77509c255d98980df416cf7ea6

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -20,4 +20,36 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
+WP* new_wp () {
+  if (free_ == NULL)
+    assert(0);
+  
+  WP* wp = free_; // erase wp from free_ link
+  free_ = free_->next;
+  wp->next = head; // insert wp into head link
+  head = wp;
 
+  return wp;
+}
+
+void free_wp (WP *wp) {
+  if (wp == NULL)
+    return;
+
+  if (head == wp) { // wp is head
+    head = wp->next;
+    wp->next = free_;
+    free_ = wp;
+  } else {
+    WP *point = head;
+    while (point->next != NULL) { // erase wp from head link
+      if (point->next == wp) {
+        point->next = wp->next;
+        wp->next = free_; // insert wp into free_ link
+        free_ = wp;
+        break;
+      }
+      point = point->next;
+    }
+  }
+}

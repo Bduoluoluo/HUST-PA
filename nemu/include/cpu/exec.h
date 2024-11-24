8854233a58ab0bde51258ee39eaf25cbc24b4660
tracer-ics2019 <tracer@njuics.org>
2024-11-24 17:01:47 +0800

    >  compile U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  17:01:47 up  4:17,  0 user,  load average: 0.31, 0.11, 0.03 6876fc1de5618567490696194e2255aa8f55de0

--- nemu/include/cpu/exec.h
+++ nemu/include/cpu/exec.h
@@ -3,8 +3,7 @@
 
 #include "nemu.h"
 #include "monitor/diff-test.h"
-// #include "rtl/rtl.h"
-#include "../src/isa/riscv32/include/isa/rtl.h"
+#include "rtl/rtl.h"
 
 #define make_EHelper(name) void concat(exec_, name) (vaddr_t *pc)
 typedef void (*EHelper) (vaddr_t *);

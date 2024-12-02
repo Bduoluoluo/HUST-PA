647796a98885d1586a02d2e20ce4aaa11027182b
tracer-ics2019 <tracer@njuics.org>
2024-12-02 15:28:21 +0800

    >  run U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  15:28:21 up  1:21,  0 user,  load average: 0.00, 0.00, 0.00 9a48a0f1407fcaea156f045debd5d76107dd9c

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,6 +3,22 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+size_t i2s (char *dst, int d, size_t n) {
+  char str[65];
+  size_t ret = 0, len = 0;
+  while (d) {
+    str[len ++] = d % 10 + '0';
+    d /= 10;
+  }
+  while (n && len) {
+    *dst = str[-- len];
+    dst ++;
+    n --;
+    ret ++;
+  }
+  return ret;
+}
+
 int printf(const char *fmt, ...) {
   return 0;
 }
@@ -12,7 +28,42 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+  va_list args;
+  size_t m = 0;
+  const char *p = fmt;
+
+  va_start(args, fmt);
+
+  while (*p != '\0') {
+    if (*p == '%') {
+      p ++;
+      switch (*p) {
+        case 'd': {
+          int num = va_arg(args, int);
+          m += i2s(out + m, num, -1);
+          break;          
+        }
+        case 's': {
+          char *s = va_arg(args, char*);
+          size_t len = strlen(s);
+          strcpy(out + m, s);
+          m += len;
+          break;          
+        }
+        case '%': {
+          out[m ++] = '%';
+          break;          
+        }
+        default: return -1;
+      }
+    } else out[m ++] = *p;
+    p ++;
+  }
+  out[m] = '\0';
+
+  va_end(args);
+
+  return m;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {

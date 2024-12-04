faf96c1bd93d387724b9b8f2dff604748f0917cf
tracer-ics2019 <tracer@njuics.org>
2024-12-05 03:05:51 +0800

    >  run U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  03:05:51 up 42 min,  1 user,  load average: 0.00, 0.01, 0.04 3e58168125663c72ccca9ea109d78f5b3247ce

--- nexus-am/am/src/nemu-common/nemu-timer.c
+++ nexus-am/am/src/nemu-common/nemu-timer.c
@@ -2,13 +2,15 @@
 #include <amdev.h>
 #include <nemu.h>
 
+static uint32_t start_time;
+
 size_t __am_timer_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_TIMER_UPTIME: {
       _DEV_TIMER_UPTIME_t *uptime = (_DEV_TIMER_UPTIME_t *)buf;
-      uint32_t t = inl(0xa1000048);
+      uint32_t t = inl(RTC_ADDR);
       uptime->hi = 0;
-      uptime->lo = t;
+      uptime->lo = t - start_time;
       return sizeof(_DEV_TIMER_UPTIME_t);
     }
     case _DEVREG_TIMER_DATE: {
@@ -26,5 +28,5 @@ size_t __am_timer_read(uintptr_t reg, void *buf, size_t size) {
 }
 
 void __am_timer_init() {
-  printf("aaaaaaaaaaa");
+  start_time = inl(RTC_ADDR);
 }

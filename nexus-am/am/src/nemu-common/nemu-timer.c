e383a2635a48ed2640de1c5c6d2c3edb04fe47be
tracer-ics2019 <tracer@njuics.org>
2024-12-05 03:04:22 +0800

    >  run U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  03:04:22 up 41 min,  1 user,  load average: 0.00, 0.02, 0.05 ab00564ecaf80cafadcae2f43b47cc80fba76f43

--- nexus-am/am/src/nemu-common/nemu-timer.c
+++ nexus-am/am/src/nemu-common/nemu-timer.c
@@ -26,4 +26,5 @@ size_t __am_timer_read(uintptr_t reg, void *buf, size_t size) {
 }
 
 void __am_timer_init() {
+  printf("aaaaaaaaaaa");
 }
--- nexus-am/tests/amtest/src/tests/rtc.c
+++ nexus-am/tests/amtest/src/tests/rtc.c
@@ -5,8 +5,6 @@ void rtc_test() {
   _DEV_TIMER_DATE_t rtc;
   int sec = 1;
   while (1) {
-    int a = uptime();
-    printf("%d\n", a);
     // while(uptime() < 1000 * sec) ;
     // get_timeofday(&rtc);
     // printf("%d-%d-%d %02d:%02d:%02d GMT (", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);

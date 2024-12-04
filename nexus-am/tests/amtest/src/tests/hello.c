049c458cffc20506b708d7c17eb82ee92e1ff91e
tracer-ics2019 <tracer@njuics.org>
2024-12-04 17:51:08 +0800

    >  run U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  17:51:08 up 51 min,  1 user,  load average: 0.17, 0.06, 0.06 2e709b7b8b223a037dade4176749e2b8108f0ca0

--- nexus-am/tests/amtest/src/tests/hello.c
+++ nexus-am/tests/amtest/src/tests/hello.c
@@ -6,7 +6,6 @@ static inline void putstr(const char *s) {
 
 void hello() {
   for (int i = 0; i < 10; i ++) {
-    printf("aaaaaaaaaaaaaaaaaaa%d%s\n", 111, "aaaaaaaaaa");
     putstr("Hello, AM World @ " __ISA__ "\n");
   }
 }

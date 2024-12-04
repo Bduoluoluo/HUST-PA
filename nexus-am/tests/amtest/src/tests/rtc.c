7cbe4d19589261e29bd1053ebe75f25328ae47ab
tracer-ics2019 <tracer@njuics.org>
2024-12-05 02:47:40 +0800

    >  run U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  02:47:40 up 24 min,  1 user,  load average: 0.13, 0.19, 0.09 52e58f53cfa03214bb3cb79fb017d1a0a177e857

--- nexus-am/tests/amtest/src/tests/rtc.c
+++ nexus-am/tests/amtest/src/tests/rtc.c
@@ -5,7 +5,8 @@ void rtc_test() {
   _DEV_TIMER_DATE_t rtc;
   int sec = 1;
   while (1) {
-    printf("%d\n", 1);
+    int a = uptime();
+    printf("%d\n", a);
     // while(uptime() < 1000 * sec) ;
     // get_timeofday(&rtc);
     // printf("%d-%d-%d %02d:%02d:%02d GMT (", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);

c44e3a2f3b8dda947ee8195cc73ed7beb49d9fdd
tracer-ics2019 <tracer@njuics.org>
2024-12-09 13:18:13 +0800

    >  compile U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  13:18:13 up  1:09,  1 user,  load average: 0.21, 0.42, 0.25 4fbd46a8d3ab88a4e2db23527e1489248e3cff92

--- nemu/src/isa/riscv32/exec/exec.c
+++ nemu/src/isa/riscv32/exec/exec.c
@@ -27,7 +27,7 @@ static OpcodeEntry irq_csr_table [8] = {
 };
 
 static make_EHelper (irq_csr) {
-  printf("aaaaaaaaaaaaaaaaaaaaaaa\n");
+  printf("aaaaaaaaaaaaaaaaaaaaaaa %d\n", decinfo.isa.instr.funct3);
   decinfo.width = irq_csr_table[decinfo.isa.instr.funct3].width;
   idex(pc, &irq_csr_table[decinfo.isa.instr.funct3]);
 }

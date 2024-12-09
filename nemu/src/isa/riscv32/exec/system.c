63bf0d133fa64242b8b97e1605c58c76d0b51197
tracer-ics2019 <tracer@njuics.org>
2024-12-09 13:28:19 +0800

    >  compile U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  13:28:19 up  1:19,  1 user,  load average: 0.26, 0.16, 0.17 fdfc11e7b23ea236622b64b571d87d91485d6510

--- nemu/src/isa/riscv32/exec/system.c
+++ nemu/src/isa/riscv32/exec/system.c
@@ -8,6 +8,7 @@ make_EHelper (csrrw) {
 
 // fun3 010
 make_EHelper (csrrs) {
+    printf("%d\n", id_src2->reg);
     rtl_or(&s0, &id_src2->val, &id_src->val);
     rtl_csr_sr(id_src2->reg, &s0, 4);
     rtl_sr(id_dest->reg, &id_src2->val, 4);

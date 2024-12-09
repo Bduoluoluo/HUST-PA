e52217cd06b54ac55ea8eb0247194c98ca26b8a3
tracer-ics2019 <tracer@njuics.org>
2024-12-09 18:26:59 +0800

    >  run U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  18:26:59 up  6:18,  1 user,  load average: 0.00, 0.00, 0.00 4e918bf4e73f63225cc99d3dabb201dae57c845

--- nexus-am/am/src/riscv32/nemu/cte.c
+++ nexus-am/am/src/riscv32/nemu/cte.c
@@ -7,6 +7,7 @@ static _Context* (*user_handler)(_Event, _Context*) = NULL;
 _Context* __am_irq_handle(_Context *c) {
   printf("%d %d %d\n", c->epc, c->cause, c->status);
   for (int i = 0; i < 32; i ++) printf("%d\n", c->gpr[i]);
+  printf("\n");
 
   _Context *next = c;
   if (user_handler) {

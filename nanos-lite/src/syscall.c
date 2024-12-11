03d9bcb220bbd0a4dc9415bd0a3cfe375cc4238c
tracer-ics2019 <tracer@njuics.org>
2024-12-11 18:19:57 +0800

    >  run U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  18:19:57 up 16 min,  1 user,  load average: 0.47, 0.18, 0.07 a3eb8860a739c62347bdfdbe7ee4c52b2c7ca8d7

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,11 +4,28 @@
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
 
   switch (a[0]) {
-    case SYS_exit: _halt(0); break;
-    case SYS_yield: _yield(); c->GPRx = 0; break;
-    default: panic("Unhandled syscall ID = %d", a[0]);
+    case SYS_exit:
+      _halt(0);
+      break;
+    case SYS_yield:
+      _yield();
+      c->GPRx = 0;
+      break;
+    case SYS_write:
+      if (a[1] == 1 || a[1] == 2) {
+        for (int i = 0; i < a[3]; i ++)
+          _putc(((uint8_t *)a[2])[i]);
+        c->GPRx = a[3];
+      } else
+        c->GPRx = -1;
+      break;
+    default:
+      panic("Unhandled syscall ID = %d", a[0]);
   }
 
   return NULL;

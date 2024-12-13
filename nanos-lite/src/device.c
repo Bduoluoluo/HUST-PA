65d6f86d8fd9a5278ed7ec449d2d419b55eca6de
tracer-ics2019 <tracer@njuics.org>
2024-12-14 01:45:51 +0800

    >  run U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  01:45:51 up  2:20,  1 user,  load average: 0.34, 0.25, 0.15 4a8f3492baa4155b8eed521315610a3ce22dd6d8

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -23,8 +23,8 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 
   _io_read(_DEV_INPUT, _DEVREG_INPUT_KBD, &kbd, sizeof(_DEV_INPUT_KBD_t));
   if (kbd.keycode != _KEY_NONE) {
-    if (kbd.keydown) sprintf(event, "kd %s\n", keyname[kbd.keydown]);
-    else sprintf(event, "ku %s\n", keyname[kbd.keydown]);
+    if (kbd.keydown) sprintf(event, "kd %s\n", keyname[kbd.keycode]);
+    else sprintf(event, "ku %s\n", keyname[kbd.keycode]);
   } else {
     _io_read(_DEV_TIMER, _DEVREG_TIMER_UPTIME, &uptime, sizeof(_DEV_TIMER_UPTIME_t));
     sprintf(event, "t %d\n", uptime.lo);    

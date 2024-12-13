ca0ee30f4d4ccf21cb1589fddcdd57ccfe37765a
tracer-ics2019 <tracer@njuics.org>
2024-12-14 00:01:02 +0800

    >  run U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  00:01:02 up 35 min,  1 user,  load average: 0.39, 0.36, 0.20 7a75cdd9fdb8810889efe1af55ffec48cb4ef20

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -61,8 +61,7 @@ size_t fs_read (int fd, void *buf, size_t len) {
 }
 
 size_t fs_write (int fd, const void *buf, size_t len) {
-
-  if (file_table[fd].write != NULL || fd == 1)
+  if (file_table[fd].write != NULL)
     return file_table[fd].write(buf, file_table[fd].open_offset, len);
 
   len = min(len, file_table[fd].size - file_table[fd].open_offset);

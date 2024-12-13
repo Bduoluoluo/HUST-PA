06759031c736a23111d17aacde7948392ea1d51d
tracer-ics2019 <tracer@njuics.org>
2024-12-13 18:08:46 +0800

    >  run U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  18:08:46 up  3:33,  1 user,  load average: 0.25, 0.11, 0.03 967377b2a1e8c0682cf955b5ee52349150b9230f

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -18,10 +18,10 @@ _Context* do_syscall(_Context *c) {
       c->GPRx = 0;
       break;
     case SYS_open:
-      c->GPRx = fs_open(a[1], a[2], a[3]);
+      c->GPRx = fs_open((const char *)a[1], a[2], a[3]);
       break;
     case SYS_read:
-      c->GPRx = fs_read(a[1], a[2], a[3]);
+      c->GPRx = fs_read(a[1], (void *)a[2], a[3]);
       break;
     case SYS_write:
       c->GPRx = -1;
@@ -30,7 +30,7 @@ _Context* do_syscall(_Context *c) {
           _putc(((uint8_t *)a[2])[i]);
         c->GPRx = a[3];
       } else {
-        c->GPRx = fs_write(a[1], a[2], a[3]);
+        c->GPRx = fs_write(a[1], (const void *)a[2], a[3]);
       }
       break;
     case SYS_close:

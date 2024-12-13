cc49bd2828d9c7ed8488853089810b4a02d3f8e6
tracer-ics2019 <tracer@njuics.org>
2024-12-13 23:53:35 +0800

    >  run U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  23:53:35 up 27 min,  1 user,  load average: 0.06, 0.12, 0.08 baa38333dc029361801ddd9840bd62def9d9b5d3

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -29,8 +29,8 @@ extern size_t serial_write (const void *buf, size_t offset, size_t len);
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
   {"stdin", 0, 0, invalid_read, invalid_write},
-  {"stdout", 0, 0, invalid_read, invalid_write},
-  {"stderr", 0, 0, invalid_read, invalid_write},
+  {"stdout", 0, 0, invalid_read, serial_write},
+  {"stderr", 0, 0, invalid_read, serial_write},
 #include "files.h"
 };
 
@@ -67,6 +67,7 @@ size_t fs_write (int fd, const void *buf, size_t len) {
     return file_table[fd].write(buf, file_table[fd].open_offset, len);
 
   len = min(len, file_table[fd].size - file_table[fd].open_offset);
+  printf("aaaaaaaaaa %d\n", fd);
   ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
   file_table[fd].open_offset += len;
   return len;

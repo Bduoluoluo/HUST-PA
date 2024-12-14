71ea3423f7a02564546f76c2ccf0faa6472ef484
tracer-ics2019 <tracer@njuics.org>
2024-12-14 15:50:03 +0800

    >  run U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  15:50:03 up  2:35,  1 user,  load average: 0.30, 0.17, 0.19 9ba427c5e0a95729c9ff7c57fb9bd53a83cb4ab

--- navy-apps/libs/libndl/src/ndl.c
+++ navy-apps/libs/libndl/src/ndl.c
@@ -133,7 +133,6 @@ static void get_display_info() {
   assert(dispinfo);
   screen_w = screen_h = 0;
   char buf[128], key[128], value[128], *delim;
-  printf("aaaaaaaaaaaaaaaa\n");
   while (fgets(buf, 128, dispinfo)) {
     *(delim = strchr(buf, ':')) = '\0';
     sscanf(buf, "%s", key);

2032911eb5bd29fc7478e1419738e17a016636a4
tracer-ics2019 <tracer@njuics.org>
2024-12-15 13:15:53 +0800

    >  run U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  13:15:53 up 58 min,  1 user,  load average: 1.05, 0.62, 0.44 2de019f8943c70c1d1593c67be95ed814f020572

--- navy-apps/libs/libndl/src/ndl.c
+++ navy-apps/libs/libndl/src/ndl.c
@@ -137,6 +137,7 @@ static void get_display_info() {
     *(delim = strchr(buf, ':')) = '\0';
     sscanf(buf, "%s", key);
     sscanf(delim + 1, "%s", value);
+    printf("%s %s\n", key, value);
     if (strcmp(key, "WIDTH") == 0) sscanf(value, "%d", &screen_w);
     if (strcmp(key, "HEIGHT") == 0) sscanf(value, "%d", &screen_h);
   }

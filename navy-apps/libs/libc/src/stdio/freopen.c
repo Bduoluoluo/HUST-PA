e7385d255f9fad2df3327284bf7b71cc6463cfe0
tracer-ics2019 <tracer@njuics.org>
2024-12-15 12:56:58 +0800

    >  run U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  12:56:58 up 39 min,  1 user,  load average: 0.46, 0.30, 0.20 a78b751078366ae74808fc6998b88e2d16ffa4fb

--- navy-apps/libs/libc/src/stdio/freopen.c
+++ navy-apps/libs/libc/src/stdio/freopen.c
@@ -217,9 +217,8 @@ _freopen_r (struct _reent *ptr,
 #endif
       return NULL;
     }
-
+printf("aaaaaaaaaaaaaaaaaa %d\n", flags);
   fp->_flags = flags;
-  printf("aaaaaaaaaaaaaaaaaa\n");
   fp->_file = f;
   fp->_cookie = (void *) fp;
   fp->_read = __sread;

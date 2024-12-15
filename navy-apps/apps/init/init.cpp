bede852b7b296cd6a255f9133dc6909626b8507b
tracer-ics2019 <tracer@njuics.org>
2024-12-15 13:03:00 +0800

    >  run U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  13:03:00 up 45 min,  1 user,  load average: 0.36, 0.43, 0.29 98d2d7eef83c7244b74e82386ecad4ccbb2416d

--- navy-apps/apps/init/init.cpp
+++ navy-apps/apps/init/init.cpp
@@ -65,7 +65,9 @@ static void clear_display();
 static void display_menu(int n);
 
 int main(int argc, char *argv[], char *envp[]) {
-  if (0) {
+  if (!freopen("/dev/tty", "r", stdin) ||
+      !freopen("/dev/tty", "w", stdout) ||
+      !freopen("/dev/tty", "w", stderr)) {
     // the OS does not meet the spec
     exit(1);
   }

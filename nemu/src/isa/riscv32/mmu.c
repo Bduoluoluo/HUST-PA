eb4c8587edec204e48cbdb8de8445b2ada6126ad
tracer-ics2019 <tracer@njuics.org>
2024-12-24 12:50:57 +0800

    >  compile U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  12:50:57 up  1:07,  1 user,  load average: 0.53, 0.13, 0.04 d8257bc517bbf90492c7ac08aaa559f7134d58b

--- nemu/src/isa/riscv32/mmu.c
+++ nemu/src/isa/riscv32/mmu.c
@@ -1,9 +1,28 @@
 #include "nemu.h"
 
+paddr_t page_translate (vaddr_t addr) {
+  uint32_t vpn1 = addr >> 22, vpn2 = (addr >> 12) & 0x3ff, offset = addr & 0xfff;
+  uint32_t pte = paddr_read((satp << 12) + vpn1 * 4, 4);
+  assert(pte & 1);
+  pte = paddr_read(((pte & ~0x3ff) << 2) + vpn2 * 4, 4);
+  assert(pte & 1);
+  return (((pte & ~0x3ff) << 2) + offset);
+}
+
 uint32_t isa_vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  if ((addr >> 12) != ((addr + len - 1) >> 12)) {
+    assert(0);
+  } else {
+    paddr_t paddr = page_translate(addr);
+    return paddr_read(paddr, len);
+  }
 }
 
 void isa_vaddr_write(vaddr_t addr, uint32_t data, int len) {
-  paddr_write(addr, data, len);
+  if ((addr >> 12) != ((addr + len - 1) >> 12)) {
+    assert(0);
+  } else {
+    paddr_t paddr = page_translate(addr);
+    paddr_write(addr, data, len);
+  }
 }

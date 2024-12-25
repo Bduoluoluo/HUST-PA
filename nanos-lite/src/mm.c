b1069a205d7a946f2bb27fce98ae888f68d3185a
tracer-ics2019 <tracer@njuics.org>
2024-12-25 15:58:52 +0800

    >  run U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  15:58:52 up  2:01,  1 user,  load average: 0.06, 0.15, 0.08 3d7231b9c156168e5e981068b73d55dfa9eb1a9f

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -20,6 +20,7 @@ extern PCB *current;
 int mm_brk(uintptr_t brk, intptr_t increment) {
   if (current->max_brk < brk) {
     for (uintptr_t i = PGROUNDUP(current->max_brk); i <= PGROUNDDOWN(brk); i += PGSIZE) {
+      printf("aaaaaaaa\n");
       uintptr_t pa = (uintptr_t)new_page(1);
       _map(&(current->as), (void *)i, (void *)pa, 0);
     }

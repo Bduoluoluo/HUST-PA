4ef2681e8d642cb92bbe7e32031b358aa7aba8a6
tracer-ics2019 <tracer@njuics.org>
2024-12-26 14:40:56 +0800

    >  run U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  14:40:56 up 29 min,  1 user,  load average: 0.44, 0.44, 0.37 acbb3b2964f5c4a3714ba9c948fdc7ff7782d2ca

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -50,7 +50,6 @@ void naive_uload(PCB *pcb, const char *filename) {
 }
 
 void context_kload(PCB *pcb, void *entry) {
-  _protect(&(pcb->as));
   _Area stack;
   stack.start = pcb->stack;
   stack.end = stack.start + sizeof(pcb->stack);

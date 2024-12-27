ab87895c959f195b2489135fe80e7aad983b3046
tracer-ics2019 <tracer@njuics.org>
2024-12-27 16:38:53 +0800

    >  run U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  16:38:53 up  2:39,  1 user,  load average: 0.19, 0.14, 0.16 4c3c4b8cdfa2665f103c003fd22812e6c531186a

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,7 +24,7 @@ extern void naive_uload (PCB *pcb, const char *filename);
 void init_proc() {
   // context_kload(&pcb[0], (void *)hello_fun);
   context_uload(&pcb[0], "/bin/pal");
-  // context_uload(&pcb[1], "/bin/hello");
+  context_uload(&pcb[1], "/bin/hello");
   switch_boot_pcb();
 
   Log("Initializing processes...");
@@ -35,8 +35,8 @@ void init_proc() {
 
 _Context* schedule(_Context *prev) {
   current->cp = prev;
-  current = &pcb[0];
-  // current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  // current = &pcb[0];
+  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
 
   return current->cp;
 }

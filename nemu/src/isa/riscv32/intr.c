b838eafc718b754cf2384b10b589dd602cb86778
tracer-ics2019 <tracer@njuics.org>
2024-12-27 16:12:23 +0800

    >  compile U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  16:12:23 up  2:13,  1 user,  load average: 0.26, 0.26, 0.20 569f53834a535c05b35d5ca19cd99853a159693b

--- nemu/src/isa/riscv32/intr.c
+++ nemu/src/isa/riscv32/intr.c
@@ -9,12 +9,13 @@ void raise_intr(uint32_t NO, vaddr_t epc) {
 
   rtl_li(&sepc, epc);
   rtl_li(&scause, NO);
-  // sstatus = (sstatus & 0xbbffffff) | ((sstatus & 0x40000000) >> 4);
+  sstatus = (sstatus & 0xbbffffff) | ((sstatus & 0x40000000) >> 4);
   sstatus = 0;
   rtl_jr(&stvec);
 }
 
 bool isa_query_intr(void) {
+  printf("aaaaaaaa\n");
   if (cpu.INTR && (sstatus & 0x40000000)) {
     cpu.INTR = false;
     raise_intr(IRQ_TIMER, cpu.pc);

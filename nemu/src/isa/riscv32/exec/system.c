b89cffe9106e6bf636707a13b47c49b6846e14a3
tracer-ics2019 <tracer@njuics.org>
2024-12-28 14:49:59 +0800

    >  compile U202114963 pa Linux debian 6.1.0-25-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.106-3 (2024-08-26) x86_64 GNU/Linux  14:49:59 up 40 min,  1 user,  load average: 0.48, 0.65, 0.55 74645e0b7140a18429a68d46955b158216fc1af

--- nemu/src/isa/riscv32/exec/system.c
+++ nemu/src/isa/riscv32/exec/system.c
@@ -9,7 +9,7 @@ make_EHelper (ecall) {
 
 make_EHelper (sret) {
     // sstatus = (sstatus & 0xffffffdd) | ((sstatus & 0x20) >> 4) | 0x20;
-    // cpu.sstatus.SIE = cpu.sstatus.SPIE;
+    cpu.sstatus.SIE = cpu.sstatus.SPIE;
     // cpu.sstatus.SPIE = 1;
     // printf("%d %d sret\n", cpu.sstatus.SIE, cpu.sstatus.SPIE);
     rtl_j(sepc + 4);
